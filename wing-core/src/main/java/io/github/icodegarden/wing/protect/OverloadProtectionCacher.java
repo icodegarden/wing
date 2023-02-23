869db70df9585c8bdea69af5d61d5c47fe26d860
徐芳方 <Fangfang.Xu@geely.com>
2023-02-23 09:04:48 +0800

    add doc

--- wing-core/src/main/java/io/github/icodegarden/wing/protect/OverloadProtectionCacher.java
+++ wing-core/src/main/java/io/github/icodegarden/wing/protect/OverloadProtectionCacher.java
@@ -5,57 +5,56 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import io.github.icodegarden.commons.lang.tuple.Tuple3;
 import io.github.icodegarden.wing.Cacher;
 import io.github.icodegarden.wing.common.RejectedRequestException;
 
 /**
- * <p>针对的是loadDB，即Supplier
- * <p>如果被拒绝性的保护，则会throws RejectedRequestException
+ * <p>
+ * 针对的是loadDB，即Supplier
+ * <p>
+ * 如果被拒绝性的保护，则会throws RejectedRequestException
+ * 
  * @author Fangfang.Xu
  *
  */
 public class OverloadProtectionCacher implements Cacher {
-	private static final Logger log = LoggerFactory.getLogger(OverloadProtectionCacher.class);
-	
+
 	private final Cacher cacher;
 	private final List<Filter> filters;
 	private final List<Protector> protectors;
-	
+
 	/**
 	 * 
 	 * @param cacher
-	 * @param filters ordered,Nullable
+	 * @param filters    ordered,Nullable
 	 * @param protectors ordered,Nullable
 	 */
-	public OverloadProtectionCacher(Cacher cacher,List<Filter> filters,List<Protector> protectors) {
-		if(cacher == null) {
+	public OverloadProtectionCacher(Cacher cacher, List<Filter> filters, List<Protector> protectors) {
+		if (cacher == null) {
 			throw new IllegalArgumentException("cacher must not null");
 		}
 		this.cacher = cacher;
 		this.filters = filters;
 		this.protectors = protectors;
 	}
-	
+
 	@Override
 	public <V> V fromSupplier(String key, Supplier<V> supplier, int expireSeconds) {
 		doFilters(key);
 		return doProtectors(key, supplier, expireSeconds);
 	}
-	
-	private void doFilters(String key) throws RejectedRequestException{
-		if(filters != null) {
-			for(Filter filter:filters) {
+
+	private void doFilters(String key) throws RejectedRequestException {
+		if (filters != null && !filters.isEmpty()) {
+			for (Filter filter : filters) {
 				filter.doFilter(key);
 			}
 		}
 	}
-	
-	private <V>V doProtectors(String key, Supplier<V> supplier, int expireSeconds){
-		if(protectors != null && !protectors.isEmpty()) {
+
+	private <V> V doProtectors(String key, Supplier<V> supplier, int expireSeconds) {
+		if (protectors != null && !protectors.isEmpty()) {
 			ProtectorChain<V> chain = new ProtectorChain.Default<V>(cacher, key, supplier, expireSeconds, protectors);
 			V v = chain.doProtector();
 			if (v != null) {
@@ -65,27 +64,32 @@ public class OverloadProtectionCacher implements Cacher {
 		}
 		return Cacher.super.fromSupplier(key, supplier, expireSeconds);
 	}
-	
+
 	@Override
 	public <V> V get(String key) {
 		return cacher.get(key);
 	}
+
 	@Override
 	public <V> Map<String, V> get(Collection<String> keys) {
 		return cacher.get(keys);
 	}
+
 	@Override
 	public <V> List<Tuple3<String, Object, Integer>> set(String key, V v, int expireSeconds) {
 		return cacher.set(key, v, expireSeconds);
 	}
+
 	@Override
 	public <V> List<Tuple3<String, Object, Integer>> set(List<Tuple3<String, V, Integer>> kvts) {
 		return cacher.set(kvts);
 	}
+
 	@Override
 	public <V> Tuple3<String, V, Integer> remove(String key) {
 		return cacher.remove(key);
 	}
+
 	@Override
 	public <V> List<Tuple3<String, V, Integer>> remove(Collection<String> keys) {
 		return cacher.remove(keys);
